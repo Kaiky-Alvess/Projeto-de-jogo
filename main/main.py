2edde9a5a574c0b551797440c5d14b489057a60d
Kaiky Alves <kaikyyalves874@gmail.com>
2026-01-30 18:13:32 -0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -28,38 +28,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-
-    circ_verde=pygame.draw.circle(janela,(0,255,0),(x_verde,y_verde),20)
-    circ_vermelho=pygame.draw.circle(janela,(255,0,0),(320,y_vermelho),20)
-    circ_amarelo=pygame.draw.circle(janela,(255,255,0),(400,500),20)
-    circ_azul=pygame.draw.circle(janela,(0,0,255),(480,500),20)
-    circ_laranja=pygame.draw.circle(janela,(255,150,0),(560,500),20)
-    seta_verde=pygame.draw.polygon(janela, (0, 255, 0), [(220, 500), (240, 450), (260, 500)])
-    seta_vermelha=pygame.draw.polygon(janela, (255, 0, 0), [(300, 500), (320, 450), (340, 500)])
-
-
-    if pygame.key.get_pressed()[K_w]:
-        seta_verde=pygame.draw.polygon(janela, (0, 150, 0), [(220, 500), (240, 450), (260, 500)])
-        if circ_verde.collidepoint(240,500):
-            pontuacao+=1
-            y_verde=0
-            print(pontuacao)
-            proxima_aparicao = randint(10, 50)
-        else:
-            pontuacao-=1
-            y_verde-=proxima_aparicao
-            proxima_aparicao = randint(10, 50)
-    if pygame.key.get_pressed()[K_a]:
-       seta_vermelha = pygame.draw.polygon(janela, (150, 0, 0), [(300, 500), (320, 450), (340, 500)])
-       if circ_vermelho.collidepoint(320, 500):
-           pontuacao += 1
-           y_vermelho = 0
-           print(pontuacao)
-           proxima_aparicao = randint(10, 50)
-       else:
-           pontuacao -= 1
-           y_vermelho -= proxima_aparicao
-           proxima_aparicao = randint(10, 50)
+    
     y_verde+=1
     y_vermelho+=1
     if y_verde>=600:
