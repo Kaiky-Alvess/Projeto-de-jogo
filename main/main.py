76d94103f0ae1c1812ddc78fe12031cadd13c5b7
Kaiky Alves <kaikyyalves874@gmail.com>
2026-01-30 19:48:57 -0300

    adicionado sistema de pontuação

--- main/main.py
+++ main/main.py
@@ -5,14 +5,18 @@ from random import randint
 
 pygame.init()
 
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.load('Thunderstruck.mp3')
+pygame.mixer.music.play()
+
+barulho_acerto= pygame.mixer.Sound('coin.wav')
 largura=800
 altura=600
 x_verde=240
 y_verde=0
-y_vermelho=0
-y_amarelo=0
-y_azul=0
-y_laranja=0
+
+fonte=pygame.font.SysFont('comicsans', 30)
+
 pontuacao=0
 
 proxima_aparicao=randint(10,50)
@@ -24,15 +28,39 @@ relogio=pygame.time.Clock()
 while True:
     janela.fill((0,0,0))
     relogio.tick(240)
+    mensagem= f'Pontos: {pontuacao}'
+    game_over= f'Fim de jogo'
+    texto= fonte.render(mensagem, True, (255,255,255), (0,0,0))
+    texto_final= fonte.render(game_over, True, (255,255,255), (0,0,0))
+
+
+    nota = pygame.draw.rect(janela, (0, 255, 0), (x_verde, y_verde, 40, 40))
+    seta = pygame.draw.rect(janela, (0, 255, 0), (x_verde, 540, 40, 40))
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    
+        if event.type == KEYDOWN:
+            if event.key == K_w:
+                if nota.colliderect(seta):
+                    barulho_acerto.play()
+                    pontuacao += 1
+                    y_verde=0
+                    y_verde-=proxima_aparicao
+                else:
+                    pontuacao -= 1
+                    y_verde = 0 - proxima_aparicao
+
+
     y_verde+=1
-    y_vermelho+=1
     if y_verde>=600:
         y_verde=0
+        pontuacao-=1
+
+    janela.blit(texto, (600, 20))
+
     pygame.display.update()
 
 
